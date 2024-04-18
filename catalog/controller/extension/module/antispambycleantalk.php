7c82d2a7c2759cf92501a1ea78b01ce5b13e5eb5
Glomberg <bazz@bk.ru>
2024-04-18 13:13:37 +0300

    Fix. Code. The repository structure changed.

---
+++ catalog/controller/extension/module/antispambycleantalk.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace catalog\controller\extension\module;
+
+use Controller;
+
+class ControllerExtensionModuleAntispamByCleantalk extends Controller
+{
+    public function index()
+    {
+        $this->load->language('extension/module/antispambycleantalk');
+
+        $data['antispambycleantalk'] = '';
+
+        return $this->load->view('extension/module/antispambycleantalk', $data);
+    }
+
+    public function check()
+    {
+        $error = false;
+
+        return !$error;
+    }
+}
